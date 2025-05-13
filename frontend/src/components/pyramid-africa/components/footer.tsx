95a3350bdd2753f21b099f7141f6b515cc221f11
boboPrem1 <boboprem1@gmail.com>
2025-05-13 11:06:37 +0000

    Footer responsive

--- frontend/src/components/pyramid-africa/components/footer.tsx
+++ frontend/src/components/pyramid-africa/components/footer.tsx
@@ -14,8 +14,8 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <div className="flex flex-col">
-      <div className="bg-[#F4FBFA] grid grid-cols-5 w-full px-4 sm:px-[52px] py-10">
-        <div className="col-span-2 flex flex-col gap-8 max-w-[400px] mb-8 sm:mb-0 w-full">
+      <div className="bg-[#F4FBFA] grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 w-full px-4 sm:px-[52px] py-10 gap-8">
+        <div className="col-span-1 sm:col-span-2 flex flex-col gap-8 max-w-[400px] mb-8 sm:mb-0 w-full">
           <img className="w-28" src={logo} alt="" />
           <p className="text-[#242827] font-normal ">
             Pyramid.Africa is your ultimate sales intelligence platform, helping
@@ -29,9 +29,9 @@ const Footer = () => {
         </div>
 
         {/* Second container */}
-        <div className=" col-span-3 grid grid-cols-4 gap-6 w-full">
+        <div className="col-span-1 sm:col-span-2 lg:col-span-3 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 w-full">
           {/* Quick Links */}
-          <div className="flex flex-col gap-6 w-full sm:w-auto">
+          <div className="flex flex-col gap-6 w-full">
             <h3 className="text-[#242827] font-medium">Quick Links</h3>
             <div className="flex flex-col gap-4">
               <h4 className="text-[#242827] font-light">Consulting</h4>
@@ -42,7 +42,7 @@ const Footer = () => {
           </div>
 
           {/* Ecosystem */}
-          <div className="flex flex-col gap-6 w-full sm:w-auto">
+          <div className="flex flex-col gap-6 w-full">
             <h3 className="text-[#242827] font-medium">Ecosystem</h3>
             <div className="flex flex-col gap-4">
               <h4 className="text-[#242827] font-light">Yprlink</h4>
@@ -53,13 +53,13 @@ const Footer = () => {
           </div>
 
           {/* Contact Us */}
-          <div className="flex flex-col gap-6 w-full sm:w-auto col-span-2">
+          <div className="flex flex-col gap-6 w-full col-span-1 sm:col-span-2">
             <h3 className="text-[#242827] font-medium">Contact Us</h3>
             <div className="flex flex-col gap-4">
-              <h4 className="text-[#242827] font-light">
+              <h4 className="text-[#242827] font-light text-sm sm:text-base">
                 Senegal | Ivory Coast | Nigeria | South Africa | Morocco | Egypt
               </h4>
-              <h4 className="text-[#242827] font-light">
+              <h4 className="text-[#242827] font-light break-words">
                 support@expandinafrica.africa
               </h4>
               <h4 className="text-[#242827] font-light">+123-456-7890</h4>
@@ -68,15 +68,15 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className="px-[52px] bg-[#F4FBFA]">
+      <div className="px-4 sm:px-[52px] bg-[#F4FBFA]">
         <hr className="h-0.5 bg-gray-300" />
       </div>
 
-      <div className="flex justify-between px-[52px] pt-8 pb-10 bg-[#F4FBFA] flex-wrap">
-        <p className="text-black font-light w-full sm:w-auto text-center sm:text-left">
+      <div className="flex flex-col sm:flex-row justify-between px-4 sm:px-[52px] pt-8 pb-10 bg-[#F4FBFA] gap-4">
+        <p className="text-black font-light text-center sm:text-left">
           © {new Date().getFullYear()} Expand in Africa. All rights reserved.
         </p>
-        <div className="flex gap-4 text-black font-light w-full sm:w-auto justify-center sm:justify-end">
+        <div className="flex gap-4 text-black font-light justify-center sm:justify-end">
           <a href="">Terms of Service</a>
           <a href="">Privacy Policy</a>
         </div>
