9e7af8ad65b6554588aa5977500441e2d4dbced2
boboprem1 <boboprem1@gmail.com>
2026-01-12 17:02:19 +0100

    Updated cors on backend express

--- backend/index.js
+++ backend/index.js
@@ -274,7 +274,11 @@ const icpsRoutes = require("./endpoints/organisations/airtableIcpRoutes.js");
 // };
 // Middleware
 //
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Authorization', 'Content-Type']
+}));
 // app.use((req, res, next) => {
 //   res.header("Access-Control-Allow-Origin", [
 //     "http://possible.africa",
